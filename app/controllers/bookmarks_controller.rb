5362c68dc669a87c20f7d3c5e6ff7b1946eddc42
Axel Bailleres <axxelb14@gmail.com>
2025-02-13 20:30:55 -0600

    new

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,7 +2,20 @@ class BookmarksController < ApplicationController
 def new
   @list = List.find(params[:list_id])
   @bookmark = Bookmark.new
-end
+  end
 
+  def create
+    @bookmark = List.new(list_params)
+    if  @bookmark.save
+      redirect_to bookmark_path(@bookmar)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
 end
